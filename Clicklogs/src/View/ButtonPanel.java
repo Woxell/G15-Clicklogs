7e997079f30ed27dc55c99dc0d8e42cfc348f681
robbieaw123 <Robert.au.williams@gmail.com>
2024-05-06 09:53:28 +0200

    KG1 Revisions
    Did the revisions deemed necessary for KG1

--- Clicklogs/src/View/ButtonPanel.java
+++ Clicklogs/src/View/ButtonPanel.java
@@ -3,13 +3,21 @@ package View;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Adds panel with buttons to MainPanel
+ * Button functionalities include: Add, Copy, Reset and Undo
+ * @author Andre, Robert (and Zahra?)
+ */
 public class ButtonPanel extends JPanel {
     private MainPanel mainPanel;
+    private int width = 500; // minimum width?
+    private int height = 50; // always same height
 
-    // private JLabel titleLeftPanel;
-    private int width = 500; //minimum width?
-    private int height = 50; //always same height
-
+    /**
+     * Constructor for ButtonPanel class
+     * @param mainPanel MainPanel
+     * @author Andre (and Zahra?)
+     */
     public ButtonPanel(MainPanel mainPanel) {
         this.mainPanel = mainPanel;
         setLayout(new FlowLayout());
@@ -18,6 +26,10 @@ public class ButtonPanel extends JPanel {
         setVisible(true);
     }
 
+    /**
+     * Initialises Add, Undo, Reset and Copy buttons and adds them to ButtonPanel
+     * @author Andre, Robert (and Zahra?)
+     */
     private void setUp() {
         JButton copy = new JButton("COPY");
         copy.setSize(width / 3, 20);
@@ -46,6 +58,11 @@ public class ButtonPanel extends JPanel {
         add(addAlt);
     }
 
+    /**
+     * Function is called when actionListeners for Add, Undo, Reset or Copy are triggered
+     * @param button Which button was pressed
+     * @author Robert
+     */
     private void buttonPressed(ButtonType button){
         mainPanel.buttonPressed(button);
     }
