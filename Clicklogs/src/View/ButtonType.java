f98890705ad7282afd54ddab38ee4f14838f9e4f
israpd <161826462+israpd@users.noreply.github.com>
2024-05-13 09:40:58 +0200

    Java_doc

--- Clicklogs/src/View/ButtonType.java
+++ Clicklogs/src/View/ButtonType.java
@@ -1,14 +1,10 @@
 package View;
 /**
-<<<<<<< Updated upstream
  * enum klass innehåller alla knappar som finns i buttonPanel
  * @author Zahraa Alqassab
- * */
-=======
- * @author Zahraa
+ *
  **/
 
->>>>>>> Stashed changes
 public enum ButtonType {
     
     UNDO,
